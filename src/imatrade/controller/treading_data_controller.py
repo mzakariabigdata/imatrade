be07bbff9a3b9f232ee38ab7ef4bf5a21f29841a
Morchid Zakaria <morchid.zakariaa@gmail.com>
2023-07-13 01:39:31 +0200

    edit file name saved

--- src/imatrade/controller/treading_data_controller.py
+++ src/imatrade/controller/treading_data_controller.py
@@ -11,12 +11,17 @@ class TreadingDataController:
     def __init__(self, data_factory):
         self.data_factory = data_factory  # Factory de données
         self.data = pd.DataFrame()  # Stocker les données du marché
+        self.file_name = None  # Stocker le nom du fichier
+
+    def get_file_name(self, file_path):
+        """Récupérer le nom du fichier"""
+        return os.path.basename(file_path[0])
 
     def load_data(self, num_files=1):
         """Charger les données du marché"""
 
-        script_path = Path(__file__).resolve() # Chemin du script
-        new_path = script_path.parent / "../../data/" # Ajouter '../../data/' 
+        script_path = Path(__file__).resolve()  # Chemin du script
+        new_path = script_path.parent / "../../data/"  # Ajouter '../../data/'
 
         # Résoudre le chemin pour éliminer le '..'
         new_path = new_path.resolve()
@@ -28,6 +33,7 @@ class TreadingDataController:
             return None
         dataframe = pd.read_excel(files[0])
         self.data = dataframe
+        self.file_name = self.get_file_name(files)
         return dataframe
 
     def print_data(self):
@@ -36,8 +42,10 @@ class TreadingDataController:
 
     def save_data(self):
         """Sauvegarder les données du marché"""
-        script_path = Path(__file__).resolve() # Chemin du script
-        save_directory = script_path.parent / "../../data/saves" # Ajouter '../../data/' 
+        script_path = Path(__file__).resolve()  # Chemin du script
+        save_directory = (
+            script_path.parent / "../../data/saves"
+        )  # Ajouter '../../data/'
 
         # Résoudre le chemin pour éliminer le '..'
         save_directory = save_directory.resolve()
@@ -46,21 +54,23 @@ class TreadingDataController:
         if not os.path.exists(save_directory):
             save_directory.mkdir(parents=True, exist_ok=True)
 
-        save_file = save_directory / "data.xlsx"
+        save_file = save_directory / self.file_name
         suffix = 0
 
         # Vérifiez si le fichier existe déjà. Dans ce cas, ajoutez un suffixe.
         while save_file.exists():
             suffix += 1
-            save_file = save_directory / f"data_{suffix}.xlsx"
+            save_file = save_directory / f"{self.file_name}_{suffix}.xlsx"
 
         if self.data.empty:
             print("No data to save.")
             return None
-        
-        # Définir le format de date
-        self.data['time'] = pd.to_datetime(self.data['time']).dt.strftime('%Y-%m-%d %H:%M:%S')
 
+        # Définir le format de date
+        self.data["time"] = pd.to_datetime(self.data["time"]).dt.strftime(
+            "%Y-%m-%d %H:%M:%S"
+        )
 
         self.data.to_excel(save_file, index=False)
-        print("Data saved to data/saves/data.xlsx")
+        print(f"Data saved to data/saves/{self.file_name}_{suffix}.xlsx")
+        return save_file
